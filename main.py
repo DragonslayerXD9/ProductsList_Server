b32488528bfe50f74b28e9c23588f0eec703e4d2
DragonslayerXD9 <73856792+DragonslayerXD9@users.noreply.github.com>
2024-12-27 22:59:24 +0100

    Finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -14,14 +15,15 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False, unique=True)
     price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String(80), nullable=False)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(120), nullable=False)
-    product_name = db.Column(db.String(120), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    products = db.Column(db.Text, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(80), nullable=False, default="Pending")
 
 @app.route('/categories', methods=['GET'])
 def get_categories():
@@ -35,6 +37,7 @@ def get_products():
         'id': p.id,
         'name': p.name,
         'price': p.price,
+        'quantity': p.quantity,
         'category': p.category
     } for p in products])
 
@@ -44,9 +47,9 @@ def get_orders():
     return jsonify([{
         'id': o.id,
         'customer_name': o.customer_name,
-        'product_name': o.product_name,
-        'quantity': o.quantity,
-        'price': o.price
+        'products': json.loads(o.products),
+        'total_price': o.total_price,
+        'status': o.status
     } for o in orders])
 
 @app.route('/products', methods=['POST'])
@@ -64,7 +67,7 @@ def add_product():
     if existing_product:
         return jsonify({'message': 'Product already exists'}), 400
 
-    product = Product(name=data['name'], price=data['price'], category=category.name)
+    product = Product(name=data['name'], price=data['price'], quantity=data['quantity'], category=category.name)
     db.session.add(product)
     db.session.commit()
     return jsonify({'message': 'Product added successfully'}), 201
@@ -72,20 +75,43 @@ def add_product():
 @app.route('/orders', methods=['POST'])
 def add_order():
     data = request.get_json()
-    product = Product.query.filter_by(name=data['product_name']).first()
-    if not product:
-        return jsonify({'message': 'Product not found'}), 404
+    
+    products_info = []
+    total_price = 0.0
+    
+    for product_data in data['products']:
+        product = Product.query.filter_by(name=product_data['name']).first()
+        
+        if not product:
+            return jsonify({'message': f"Product {product_data['name']} not found"}), 404
+        product_info = {
+            "name": product.name,
+            "quantity": product_data['quantity'],
+            "price": product.price,
+            "total_price": product.price * product_data['quantity']
+        }
+        
+        products_info.append(product_info)
+        
+        total_price += product_info['total_price']
+        
+        db.session.commit()
+
     order = Order(
         customer_name=data['customer_name'],
-        product_name=data['product_name'],
-        quantity=data['quantity'],
-        price=product.price
+        products=json.dumps(products_info),
+        total_price=total_price,
+        status='Complete'
     )
+
     db.session.add(order)
     db.session.commit()
-    return jsonify({'message': 'Order added successfully'}), 201
+
+    return jsonify({'message': 'Order added successfully', 'order_id': order.id, 'total_price': total_price}), 201
+
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Recreate tables based on the current models
-    app.run(debug=True)
+        # db.drop_all()
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
